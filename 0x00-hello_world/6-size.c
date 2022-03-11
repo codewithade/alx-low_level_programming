68957e0e9d6a36a65668007011fefab85d58b616
root <codewithade@gmail.com>
2022-03-10 16:47:56 -0800

    betty o

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ printf("%s %d %s\n", "Size of a char:", sizeof(char), "byte(s)");
 printf("%s %d %s\n", "Size of an int:", sizeof(int), "byte(s)");
 printf("%s %d %s\n", "Size of a long int:", sizeof(long int), "byte(s)");
 printf("%s %d %s\n", "Size of a long long int:", 
-		sizeof(long long int), "byte(s)");
+sizeof(long long int), "byte(s)");
 printf("%s %d %s\n", "Size of a float:", sizeof(float), "byte(s)");
 return (0);
 }
