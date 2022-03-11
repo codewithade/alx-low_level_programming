c1ba28a2f83506b8adb2f8f9106f059576f32953
root <codewithade@gmail.com>
2022-03-10 16:42:52 -0800

    updated file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
- * */
+ */
 int main(void)
 {
 printf("%s %d %s\n", "Size of a char:", sizeof(char), "byte(s)");
