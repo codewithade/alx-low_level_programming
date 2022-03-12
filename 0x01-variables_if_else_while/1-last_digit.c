dfb56a64ad33e37040dbf1a960ac8495978189e2
root <codewithade@gmail.com>
2022-03-12 07:47:01 -0800

    fixed betty issues

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -12,18 +12,18 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 char s[] = "Last digit of";
-int l = abs(n%10);
-if(l>5)
+int l = abs(n % 10);
+if (l > 5)
 {
-printf("%s %d is %d and is greater than 5\n", s,n,l);
+printf("%s %d is %d and is greater than 5\n", s, n, l);
 }
-else if(l==0)
+else if (l == 0)
 {
-printf("%s %d is %d and is 0\n", s,n,l);
+printf("%s %d is %d and is 0\n", s, n, l);
 }
-else if(l<6 && l!=0)
+else if (l < 6 && l != 0)
 {
-printf("%s %d is %d and is less than 6 and not 0\n",s,n,l);
+printf("%s %d is %d and is less than 6 and not 0\n", s, n, l);
 }
 return (0);
 }
