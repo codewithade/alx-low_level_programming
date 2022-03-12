1013dd42a22836947b0914167f1b050655d6a678
root <codewithade@gmail.com>
2022-03-12 08:09:57 -0800

    added few changes

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int n = 0;
-for(;n<10;n++)
-	printf("%d",n);
+int n;
+
+n = 0;
+for (; n < 10; n++)
+	printf("%d", n);
 putchar('\n');
 return (0);
 }
