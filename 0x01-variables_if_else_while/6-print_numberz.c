72dd9aeaa74fcbdc5af391426cf00bd981f92260
root <codewithade@gmail.com>
2022-03-12 08:11:43 -0800

    addd few changes

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int n = 0;
-for(;n<10;n++)
-	putchar(n+'0');
+int n;
+
+n = 0;
+for (; n < 10; n++)
+	putchar(n + '0');
 putchar('\n');
 return (0);
 }
