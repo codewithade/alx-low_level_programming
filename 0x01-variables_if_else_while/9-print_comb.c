e97d8d891e2509a9dbb9badb1ce6c9b547fdf730
root <codewithade@gmail.com>
2022-03-12 08:18:03 -0800

    added some changes

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int n=0;
-for(;n<=9;n++)
+int n;
+
+n = 0;
+for (; n <= 9; n++)
 {
-putchar(n+'0');
-if(n==9)
+putchar(n + '0');
+if (n == 9)
    break;
 putchar(',');
 putchar(' ');
