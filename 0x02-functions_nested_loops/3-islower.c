6caa7b2235712c3b4a3f3ecc5baadb1242fec4a3
root <codewithade@gmail.com>
2022-03-15 06:15:06 -0700

    added 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * _islower - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+int i;
+
+c = c + '0';
+for (i = 'a'; i <= 'z'; i++)
+{
+if (i == c)
+{
+return(1);
+}
+}
+return (0);
+}
