2b0f050fedf3089e4ca3fbda9483804a8f6022ab
root <codewithade@gmail.com>
2022-03-15 07:34:26 -0700

    fixed betty style

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,8 @@
 /**
  * _islower - Entry point
  *
+ * @c: Number to pass to _islower function
+ *
  * Return: Always 0 (Success)
  */
 int _islower(int c)
@@ -9,9 +11,9 @@ int _islower(int c)
 int i;
 
 for (i = 'a'; i <= 'z'; i++)
-{
+i{
 if (i == c)
-	return(1);
+	return (1);
 }
 return (0);
 }
