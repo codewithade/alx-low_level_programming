7cba3e7215ecdf70704fbb0947191506a2e7551e
root <codewithade@gmail.com>
2022-03-15 08:50:39 -0700

    editted script

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,16 +11,19 @@ int print_sign(int n)
 if (n == 0)
 {
 	putchar('0');
+	putchar('0');
 	return (0);
 }
 else if (n < 0)
 {
 	putchar('-');
+	putchar('1');
 	return (-1);
 }
 else
 {
 	putchar('+');
+	putchar('1');
 	return (1);
 }
 }
