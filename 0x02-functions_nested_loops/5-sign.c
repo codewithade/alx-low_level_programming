7ef6be70935bf6cb2cac045ae31e22494e5aa998
root <codewithade@gmail.com>
2022-03-15 08:59:16 -0700

    revert back toc orrect code

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,21 +6,21 @@
  *
  * Return: Always 0 (Success)
  */
-void print_sign(int n)
+int print_sign(int n)
 {
 if (n == 0)
 {
 	putchar('0');
-	putchar('0');
+	return (0);
 }
 else if (n < 0)
 {
 	putchar('-');
-	putchar('1');
+	return (-1);
 }
 else
 {
 	putchar('+');
-	putchar('1');
+	return (1);
 }
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,4 +3,4 @@ void print_alphabet_x10(void);
 int _islower(int c);
 int _putchar(int c);
 int _isalpha(int c);
-void print_sign(int n);
+int print_sign(int n);
