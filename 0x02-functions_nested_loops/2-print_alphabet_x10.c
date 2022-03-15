e4c64a3df0b03765a8344af56558048499413fd5
root <codewithade@gmail.com>
2022-03-15 05:41:34 -0700

    added2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+print_alphabet();
+}
+}
