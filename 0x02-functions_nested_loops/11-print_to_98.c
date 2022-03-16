f7b62965a7390c72963bf608945cd83bd24c2c3c
root <codewithade@gmail.com>
2022-03-16 07:49:23 -0700

    added 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_to_98 - Entry point
+ *
+ * @n: Second Number to add
+ *
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+int MAX;
+
+MAX = 98;
+for (; n <= MAX; n++)
+{
+if (n == MAX)
+	printf("%d\n", n);
+else
+	printf("%d, ", n);
+}
+}
