6ad5191d3d5ef1d1858777e8751f50c2fce4b66d
root <codewithade@gmail.com>
2022-03-16 04:24:39 -0700

    added 9-times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * times_table - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+int r, c, res;
+
+for (c = 0; c <= 9; c++)
+{
+for (r = 0; r <= 9; r++)
+{
+res = c * r;
+print_num(res);
+if (r < 9)
+{
+putchar(',');
+putchar(' ');
+putchar(' ');
+}
+}
+putchar('\n');
+}
+}
+
+void print_num(int n)
+{
+if (n < 0)
+{
+putchar('-');
+n = -n;
+}
+if (n / 10)
+	print_num(n / 10);
+
+putchar(n % 10 + '0');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,3 +7,5 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
+void times_table(void);
+void print_num(int n);
