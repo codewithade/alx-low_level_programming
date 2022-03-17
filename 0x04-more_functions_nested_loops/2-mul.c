90b3330f8539340a4cd73c6fb75ac32c7009443c
root <codewithade@gmail.com>
2022-03-17 05:20:11 -0700

    added 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * mul - Entry point
+ *
+ * @a: first Number to compute
+ *
+ * @b: second number to compute
+ *
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
