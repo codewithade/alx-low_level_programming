ad09fcb3e7d82a9e839efac70b837ae546c48dbb
root <codewithade@gmail.com>
2022-03-18 14:42:51 -0700

    added some corrections

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * more_numbers - Entry point
  *
@@ -10,7 +11,7 @@ int i, j;
 
 for (j = 0; j < 10; j++)
 {
-for (i = 0; i < 14; i++)
+for (i = 0; i <= 14; i++)
 {
 print_num(i);
 }
