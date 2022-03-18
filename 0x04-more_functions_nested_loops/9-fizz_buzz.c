f173c5960a72f75c1bf86da1f43b49d1c68e13ae
root <codewithade@gmail.com>
2022-03-18 13:50:53 -0700

    added fizzbuzz program

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+fizz_buzz();
+}
+/**
+ * fizz_buzz - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void fizz_buzz(void)
+{
+int i;
+
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+	printf("%s ", "FizzBuzz");
+else if (i % 3 == 0)
+	printf("%s ", "Fizz");
+else if (i % 5 == 0)
+	printf("%s ", "Buzz");
+else
+	printf("%d ", i);
+}
+putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,3 +9,4 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_space(int n);
 void print_square(int size);
+void fizz_buzz(void);
