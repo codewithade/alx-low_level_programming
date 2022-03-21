70a35b7c6730d873ebf5530c4783e2968924a5ac
root <codewithade@gmail.com>
2022-03-21 16:48:29 -0700

    added 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - reset a pointer
+ *
+ * @a: pointer to integer array
+ *
+ * @n: number of elements to evaluate
+ *
+ * Return: Always 0 (Success)
+ */
+void print_array(int *a, int n)
+{
+int i;
+
+for (i = 0 ; i < n - 1; i++)
+{
+printf("%d, ", *(a + i));
+}
+printf("%d\n", *(a + i));
+}
+
+/**
+ * _strlen - return string lenght
+ *
+ * @s: pointer to the string
+ *
+ * Return: Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+int i, count;
+
+i = 0;
+count = 0;
+while (*(s + i) != '\0')
+{
+count++;
+i++;
+}
+return (count);
+}
