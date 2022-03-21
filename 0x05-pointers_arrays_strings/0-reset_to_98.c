a5dcb89ca71e17d4f9f994c203df98f290394faa
root <codewithade@gmail.com>
2022-03-21 10:08:21 -0700

    added 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - reset a pointer
+ *
+ * @n: pointer to evaluate
+ *
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+All about pointers, arrays and strings
