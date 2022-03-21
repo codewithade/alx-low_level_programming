0d05f8b282fd3123df235361b5612cc5fd1818ce
root <codewithade@gmail.com>
2022-03-21 12:52:33 -0700

    added 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * _strlen - reset a pointer
+ *
+ * @s: pointer to evaluate
+ *
+ * Return: Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+int i, count;
+
+i = 0;
+count = 0;
+while (*(s + i) != '\0')
+{
+count++;
+i++;
+}
+return (count);
+}
