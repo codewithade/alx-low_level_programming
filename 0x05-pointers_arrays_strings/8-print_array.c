1bbc81b1d58f44bd9ff3825e87aa12758c443a7e
root <codewithade@gmail.com>
2022-03-21 16:53:43 -0700

    corrected code

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,11 +13,18 @@ void print_array(int *a, int n)
 {
 int i;
 
+if (n > 0)
+{
 for (i = 0 ; i < n - 1; i++)
 {
 printf("%d, ", *(a + i));
 }
 printf("%d\n", *(a + i));
+}
+else
+{
+putchar('\n');
+}
 }
 
 /**
