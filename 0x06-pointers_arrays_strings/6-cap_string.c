1adea87755e945c0e20a50124b000d7985cafe3a
root <codewithade@gmail.com>
2022-03-24 09:26:02 -0700

    added 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,16 +11,28 @@ char *cap_string(char *s)
 {
 int i, len;
 char upper[1024];
+char c;
 
 len =  _strlen(s);
 i = 0;
-
-while (*(s + i) != '\0')
+c = *(s + i);
+while (c != '\0' && i < len - 1)
 {
-upper[i] = _toupper(*(s + i));
+c = *(s + i);
+if (_isspecial(c) == '1' && _isspecial(*(s + i + 1)) != '1')
+{
+upper[i] = c;
+upper[i + 1] = _toupper(*(s + i + 1));
+i += 2;
+}
+else
+{
+upper[i] = c;
 i++;
 }
-upper[i] = '\0';
+}
+upper[i] = _toupper(*(s + i));
+upper[i + 1] = '\0';
 
 for (i = 0; i < len; i++)
 	*(s + i) = upper[i];
@@ -35,16 +47,30 @@ return (s);
  */
 char _isspecial(char c)
 {
-char sp[100];
+char sp[14];
 int i;
 
 i = 0;
-sp = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\0'};
+sp[0] = ' ';
+sp[1] = '\t';
+sp[2] = '\n';
+sp[3] = ',';
+sp[4] = ';';
+sp[5] = '.';
+sp[6] = '!';
+sp[7] = '?';
+sp[8] = '"';
+sp[9] = '(';
+sp[10] = ')';
+sp[11] = '{';
+sp[12] = '}';
+sp[13] = '\0';
 
 while(*(sp + i) != '\0')
 {
 if (c == *(sp + i))
-       return '1';	
+       return ('1');
+i++;
 }
 return c;
 }
