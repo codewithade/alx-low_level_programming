16882a1da5efafcc9d4f0476a97193033b95e263
root <codewithade@gmail.com>
2022-04-02 07:21:51 -0700

    added 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - entry point to program
+ *
+ * @argc: commad line args count
+ *
+ * @argv: arrays of command ine args
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
