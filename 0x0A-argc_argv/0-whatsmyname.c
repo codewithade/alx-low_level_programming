2336980ce5093879794e3e5e3c34b6b7168164d0
root <codewithade@gmail.com>
2022-04-02 07:15:00 -0700

    added 0x0A-argc_argv and 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - entry point to program
+ *
+ * @argc: commad line args count
+ *
+ * @argv: arrays of command ine args
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
