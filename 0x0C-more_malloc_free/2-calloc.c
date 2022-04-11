15d69b8fc597c8f4da2ff20c82d5c0ae6de7b6a2
root <codewithade@gmail.com>
2022-04-11 08:20:51 -0700

    added 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _calloc - allocates memory of size
+ *
+ * @nmemb: numb of memory
+ *
+ * @size: size of eachmem
+ *
+ * Return: void pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+unsigned int i;
+char *p;
+
+if (nmemb == 0 || size == 0)
+	return (NULL);
+p = malloc(nmemb * size);
+if (p == NULL)
+	return (NULL);
+for (i = 0; i < nmemb; i++)
+	*(p + i) = 0;
+return (p);
+}
