fc98d14709bd80a564e26e2254d6c8d70263c3c6
root <codewithade@gmail.com>
2022-04-13 05:47:49 -0700

    added and updated 5-free_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,4 +17,5 @@ float age;
 dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+void free_dog(dog_t *d);
 #endif
