9cd98bf97945048b82aecf5970917c54f288ce28
root <codewithade@gmail.com>
2022-04-13 09:41:56 -0700

    added null check

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -11,5 +11,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+if (f == NULL)
+	return;
 f(name);
 }
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,6 +15,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t i;
 
+if (action == NULL)
+	return;
 i = 0;
 while (i < size)
 {
