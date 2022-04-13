06a91095030822ffa33d4fe4313f24c26cb7c359
root <codewithade@gmail.com>
2022-04-13 09:16:24 -0700

    created Directory: 0x0F-function_pointers File: 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_name - prints a name
+ *
+ * @name: name to print
+ * @f: pointer to function
+ *
+ * Return: nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+All about pointers to functions
