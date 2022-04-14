81658287a709d1349328ae7acd56f908b0d13cea
root <codewithade@gmail.com>
2022-04-14 01:20:59 -0700

    updated errors

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,7 +15,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
 
-if (cmp == NULL || size <= 0)
+if (cmp == NULL || array == NULL || size <= 0)
 	return (-1);
 
 for (i = 0; i < size; i++)
