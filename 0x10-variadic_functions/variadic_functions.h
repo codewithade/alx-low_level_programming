5decc5f267fc616914da28f6d4312c4781b0b92f
root <codewithade@gmail.com>
2022-04-15 09:19:15 -0700

    added define xtics

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,3 +1,5 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
 #include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
@@ -5,3 +7,4 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _strlen(const char * const s);
 void print_arg(va_list ap, char *separator, char type);
+#endif
