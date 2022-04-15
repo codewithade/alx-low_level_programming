84bcb4d2764f1920c7fb9a732eb627b94b45fb5f
root <codewithade@gmail.com>
2022-04-15 01:27:20 -0700

    updated and removed if stmnt

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,7 +12,7 @@
  */
 int main(int argc, char *argv[])
 {
-int num1, num2, result;
+int num1, num2, result, state;
 char *operator, *add, *sub, *div, *mul, *mod;
 
 if (argc < 4)
@@ -25,11 +25,19 @@ num1 = atoi(*(argv + 1));
 operator = *(argv + 2);
 num2 = atoi(*(argv + 3));
 
+state = (num2 == 0) ? 0 : 1;
+
 add = "+";
 sub = "-";
 div = "/";
 mul = "*";
 mod = "%";
+
+if ((state == 0) && (strcmp(operator, div) == 0 || strcmp(operator, mod) == 0))
+{
+        printf("Error\n");
+        return (100);
+}
 
 if (strcmp(operator, add) == 0)
 {
@@ -48,21 +56,11 @@ else if (strcmp(operator, mul) == 0)
 }
 else if (strcmp(operator, div) == 0)
 {
-	if (num2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	result = get_op_func(div)(num1, num2);
 	printf("%d\n", result);
 }
 else if (strcmp(operator, mod) == 0)
 {
-	if (num2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	result = get_op_func(mod)(num1, num2);
 	printf("%d\n", result);
 }
