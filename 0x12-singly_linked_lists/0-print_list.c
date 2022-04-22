706cabebade12daf48af3777e6c5aed0a0247826
root <codewithade@gmail.com>
2022-04-22 01:38:48 -0700

    added more changes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,9 +14,13 @@ nodes = 0;
 while (h != NULL)
 {
 if (h->str == NULL)
-	printf("[0] (nil)\n");
+{
+printf("[0] (nil)\n");
+}
 else
-	printf("[%d] %s\n", h->len, h->str);
+{
+printf("[%d] %s\n", h->len, h->str);
+}
 h = h->next;
 nodes++;
 }
