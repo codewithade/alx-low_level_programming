9f2b67d42c690aea299b902a09e40a518f8b34e0
root <codewithade@gmail.com>
2022-04-25 02:36:45 -0700

    added and updated Directory: 0x13-more_singly_linked_lists File: 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * print_listint - prints all the elements of a listint_t
+ *
+ * @h: pointer to the listint_t structure
+ *
+ * Return: number of nodes in struct list
+ */
+size_t print_listint(const listint_t *h)
+{
+int nodes;
+
+nodes = 0;
+
+while (h != NULL)
+{
+	nodes++;
+	h = h->next;
+}
+
+return (nodes);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+More advanced singly linked lists
