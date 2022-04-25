e713257d9c6d3fb35597de2dec7abf89c5b3baac
root <codewithade@gmail.com>
2022-04-25 04:22:26 -0700

    frredd memory

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a listint_t and sets the head to NULL
+ *
+ * @head: pointer to head of node
+ *
+ * Return: Nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while ((*head)->next != NULL)
+	{
+		temp = *head;
+		*head = temp->next;
+		free(temp);
+	}
+	free(*head);
+	head = NULL;
+}
