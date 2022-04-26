e7eee63451f37dec6e10b6bd845c35d9ccba3691
root <codewithade@gmail.com>
2022-04-26 10:32:40 -0700

    made some corrections

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 listint_t *last, *prev, *current, *next;
 unsigned int count;
 
-if (head == NULL || *head == NULL)
+if (head == NULL || (*head) == NULL)
 	return (-1);
 
 count = 0;
