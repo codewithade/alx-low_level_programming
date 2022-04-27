f58d376955e280963d7df1e73891234e311c3cee
root <codewithade@gmail.com>
2022-04-27 06:31:11 -0700

    changes made

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,16 +9,17 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+
 	if (head != NULL && (*head) != NULL)
 	{
-		while ((*head)->next != NULL)
-		{
-			temp = *head;
-			*head = temp->next;
-			free(temp);
-		}
-		free(*head);
-		*head = NULL;
-		head = NULL;
+	while ((*head)->next != NULL)
+	{
+		temp = *head;
+		*head = temp->next;
+		free(temp);
+	}
+	free(*head);
+	*head = NULL;
+	head = NULL;
 	}
 }
